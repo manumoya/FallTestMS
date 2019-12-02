bb394bed1ceed5cc9661c9e35b9d891122518bd3
Manuel Moya <mmoyam@gmail.com>
2019-12-02 15:42:34 -0300

    Refactoring BeerDAO
    Refactoring BeerDAO

--- src/main/java/fallfin/dao/BeerDAO.java
+++ src/main/java/fallfin/dao/BeerDAO.java
@@ -6,17 +6,17 @@ import fallfin.module.BeerItem;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BeerDAOImpl implements IBeerDAO {
+public class BeerDAO {
 
     private static List<BeerItem> beers = new ArrayList();
 
     /* Busca todas las cervezas */
-    public List<BeerItem> findAll(){
+    public static List<BeerItem> findAll(){
         return beers;
     }
 
     /* graba una cerveza */
-    public int save (BeerItem beer){
+    public static Integer save (BeerItem beer){
         Integer id = findAll().size()+1;
         beer.setId(id);
         beers.add(beer);
