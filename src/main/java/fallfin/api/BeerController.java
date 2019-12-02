ce33941e121b638d8c93ff27dfd53612f07b02b4
Manuel Moya <mmoyam@gmail.com>
2019-12-02 12:28:38 -0300

    refactoring BeerDAO
    refactoring BeerDAO

--- src/main/java/fallfin/api/BeerController.java
+++ src/main/java/fallfin/api/BeerController.java
@@ -1,5 +1,9 @@
 package fallfin.api;
 
+import fallfin.dao.BeerDAOImpl;
+import fallfin.module.BeerItem;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,20 @@ public class BeerController {
         return "Gracias por llegar a la API de Manuel Moya para FallFin";
     }
 
+
+    /* POST => Grabar cerveza */
+    @PostMapping("/beers")
+    public BeerItem addBeers(@RequestBody BeerItem beer) {
+
+        /*
+        BeerDAOImpl beerBoxDao = new BeerDAOImpl();
+
+        int id = PersonaInfoService.savePersona(persona);
+        return persona;
+
+         */
+
+        return beer;
+    }
 
 }
--- src/test/java/fallfin/dao/BeerBoxDAOTest.java
+++ src/test/java/fallfin/dao/BeerBoxDAOTest.java
@@ -12,8 +12,8 @@ import static org.junit.Assert.assertEquals;
 public class BeerBoxDAOTest {
     @Test
     public void findAll() {
-        BeerBoxDAOImpl beerBoxDao = new BeerBoxDAOImpl();
-        List<BeerItem> list = beerBoxDao.findAll();
+        BeerDAOImpl beerDao = new BeerDAOImpl();
+        List<BeerItem> list = beerDao.findAll();
         assertEquals(0, list.size());
     }
 
@@ -23,9 +23,9 @@ public class BeerBoxDAOTest {
         BeerBox beerBox = new BeerBox(3000.00);
         BeerItem beer = new BeerItem(0, "pilsen", "ARTOR", "Chile", 1000.00, "CLP", beerBox);
 
-        BeerBoxDAOImpl beerBoxDao = new BeerBoxDAOImpl();
-        beerBoxDao.save(beer);
-        List<BeerItem> beers = beerBoxDao.findAll();
+        BeerDAOImpl beerDao = new BeerDAOImpl();
+        beerDao.save(beer);
+        List<BeerItem> beers = beerDao.findAll();
         assertEquals(1,beers.size());
 
 
