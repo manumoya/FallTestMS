48b3bdb96eff610a7be4ca36639b518e9d33169d
Manuel Moya <mmoyam@gmail.com>
2019-12-03 05:50:33 -0300

    Crear bean BeerBoxBuy.java
    Crear bean BeerBoxBuy.java

--- src/main/java/fallfin/api/BeerBoxController.java
+++ src/main/java/fallfin/api/BeerBoxController.java
@@ -1,10 +1,33 @@
 package fallfin.api;
 
+import fallfin.dao.BeerDAO;
+import fallfin.module.BeerBox;
+import fallfin.module.BeerBoxBuy;
+import fallfin.module.BeerItem;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BeerBoxController {
 
-
+    /* GET => Buscar BOX Price */
+    @GetMapping("/beers/{beerID}/boxprice")
+    public ResponseEntity<BeerBox> searchBeerById(@PathVariable Integer beerID, @RequestBody BeerBoxBuy beerBoxBuy){
+        try {
+            BeerItem beerFound = BeerDAO.get( beerID );
+            BeerBox beerBox = new BeerBox(1243.89);
+            if(beerFound.getId() != null) { // Se encontró
+                return new ResponseEntity<BeerBox>(beerBox, HttpStatus.OK); // Code 200
+            }else{
+                return new ResponseEntity<BeerBox>(beerBox, HttpStatus.NOT_FOUND); // Code 404
+            }
+        }catch( Exception e){
+            return new ResponseEntity<BeerBox>(new BeerBox(), HttpStatus.BAD_REQUEST); // Code 400
+        }
+    }
 
 }
---
+++ src/main/java/fallfin/module/BeerBoxBuy.java
@@ -0,0 +1,27 @@
+package fallfin.module;
+
+public class BeerBoxBuy {
+
+    private String currency;
+    private Integer quantity =6;
+
+    public BeerBoxBuy(){
+
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuanty(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
