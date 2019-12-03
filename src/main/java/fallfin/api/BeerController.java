6ac65e0d6e70f9db09ad11dd879757ecb0618549
Manuel Moya <mmoyam@gmail.com>
2019-12-02 22:06:39 -0300

    Agregar funcionalidad BeerController.searchBeerById()
    Agregar funcionalidad BeerController.searchBeerById()

--- src/main/java/fallfin/api/BeerController.java
+++ src/main/java/fallfin/api/BeerController.java
@@ -2,12 +2,13 @@ package fallfin.api;
 
 import fallfin.dao.BeerDAO;
 import fallfin.module.BeerItem;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 public class BeerController {
@@ -19,27 +20,52 @@ public class BeerController {
     }
 
 
-    /* POST => Grabar cerveza */
+    /* POST => Ingresa una nueva cerveza */
     @PostMapping("/beers")
     public ResponseEntity<String> addBeers(@RequestBody BeerItem beer) {
-
         String msgOut ="Request inválida";
         HttpStatus statusHttp = HttpStatus.BAD_REQUEST; // 400;
 
         BeerItem beerFound = BeerDAO.get( beer.getId() );
 
-        /* Cerveza no encontrada, graba cerveza */
-        if (beerFound.getId() == null){
+        if (beerFound.getId() == null){ // Cerveza ID  no encontrada, graba cerveza
             int id = BeerDAO.save(beer);
             msgOut ="Cerveza creada";
             statusHttp = HttpStatus.CREATED; // 201
-        }else{
+        }else{ // Grabar Cerveza
             msgOut ="El ID "+ beer.getId() +" de la cerveza ya existe";
             statusHttp = HttpStatus.CONFLICT; //409
         }
 
         return new ResponseEntity<String>(msgOut, statusHttp);
-
     }
+
+    /* GET => Buscar Cerveza por ID */
+    @GetMapping("/beers/{beerID}")
+    public ResponseEntity<String> searchBeerById(@PathVariable Integer beerID){
+        String msgOut ="El Id de la cerveza no existe";
+        HttpStatus statusHttp = HttpStatus.NOT_FOUND; // 404;
+
+        BeerItem beerFound = BeerDAO.get( beerID );
+
+        if(beerFound.getId() != null) {
+            msgOut ="Operacion exitosa";
+            statusHttp = HttpStatus.OK; // 200;
+
+            URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/components/schemas/BeerItem")
+                    .buildAndExpand()
+                    .toUri();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setLocation(location);
+
+            return new ResponseEntity<String>(msgOut, headers,  statusHttp);
+
+        }
+
+        return new ResponseEntity<String>(msgOut, statusHttp);
+    }
+
 
 }
