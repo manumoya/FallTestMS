25c466976a3163c281382110a347c0205a40a0c8
Manuel Moya <mmoyam@gmail.com>
2019-12-03 06:59:47 -0300

    Agregar validación de caja con 6 botellas
    Agregar validación de caja con 6 botellas

--- src/main/java/fallfin/api/BeerBoxController.java
+++ src/main/java/fallfin/api/BeerBoxController.java
@@ -4,6 +4,7 @@ import fallfin.dao.BeerDAO;
 import fallfin.module.BeerBox;
 import fallfin.module.BeerBoxBuy;
 import fallfin.module.BeerItem;
+import fallfin.service.Validation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,14 +22,21 @@ public class BeerBoxController {
             BeerItem beerFound = BeerDAO.get( beerID );
 
 
-            BeerBox beerBox = new BeerBox(1243.89);
-
-
-
             if(beerFound.getId() != null) { // Se encontró
+
+                /* Corrige cantidad minima */
+                Integer quantityOK = Validation.getQuantityBeerOK(beerBoxBuy.getQuantity());
+                beerBoxBuy.setQuanty(quantityOK);
+
+                /* Calcula el monto total */
+                Double montoTotal = beerFound.getPrice()*beerBoxBuy.getQuantity();
+
+                /**/
+                BeerBox beerBox = new BeerBox( montoTotal );
+
                 return new ResponseEntity<BeerBox>(beerBox, HttpStatus.OK); // Code 200
             }else{
-                return new ResponseEntity<BeerBox>(beerBox, HttpStatus.NOT_FOUND); // Code 404
+                return new ResponseEntity<BeerBox>(new BeerBox(), HttpStatus.NOT_FOUND); // Code 404
             }
         }catch( Exception e){
             return new ResponseEntity<BeerBox>(new BeerBox(), HttpStatus.BAD_REQUEST); // Code 400
